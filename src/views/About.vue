2949955c18f09759736334d628598ea9bdfb61c2
Thomas Vionnet <th.vionnet@gmail.com>
2021-01-06 16:40:27 +0100

    (feat): archi_project
    add API call in About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>{{ data }}</h1>
   </div>
 </template>
+
+<script>
+import axios from '../tools/axios';
+
+export default {
+  data() {
+    return {
+      data: ""
+    };
+  },
+
+  beforeMount() {
+    this.getMsg();
+  },
+  
+  methods: {
+    async getMsg(){
+      const res = await axios.get('/');
+      console.log(res);
+      const data = await res.data;
+      this.data = data;
+    }
+  }
+};
+</script>
