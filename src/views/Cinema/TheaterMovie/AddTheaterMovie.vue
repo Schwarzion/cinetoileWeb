f783ccd6948d9e37850dabf41303fc6df0734abb
Laurene Flamey <laurene.flamey@live.fr>
2021-05-26 18:37:29 +0200

    (fix) : catch error

--- src/views/Cinema/TheaterMovie/AddTheaterMovie.vue
+++ src/views/Cinema/TheaterMovie/AddTheaterMovie.vue
@@ -19,7 +19,11 @@
 </template>
 
 <script>
-import { addTheaterMovie, editTheaterMovie } from "@/services/theaterMovie.service";
+import {
+  addTheaterMovie,
+  editTheaterMovie,
+} from "@/services/theaterMovie.service";
+import { displayError } from "@/tools/displayError";
 
 export default {
   props: ["movie", "theaterMovieId"],
@@ -27,12 +31,11 @@ export default {
   data() {
     return {
       errors: [],
-    
+
       dateRange: {
         start: (this.movie && this.movie.startDate) || new Date(),
-        end: (this.movie && this.movie.endDate) || new Date()
+        end: (this.movie && this.movie.endDate) || new Date(),
       },
-
     };
   },
 
@@ -40,39 +43,43 @@ export default {
     checkForm() {
       this.errors = [];
 
-      !this.dateRange.start && this.errors.push('Date de début requise.');
-      !this.dateRange.end && this.errors.push('Date de fin requise.');
+      !this.dateRange.start && this.errors.push("Date de début requise.");
+      !this.dateRange.end && this.errors.push("Date de fin requise.");
     },
 
     async saveTheaterMovie() {
       const theaterMovie = {
-        theaterId: 2, 
-        movieId: this.movie.movieId || this.movie.id, 
+        theaterId: 2,
+        movieId: this.movie.movieId || this.movie.id,
         startDate: this.dateRange.start,
         endDate: this.dateRange.end,
-        status: true, 
-        theaterFav: false  
-      }
+        status: true,
+        theaterFav: false,
+      };
 
-      try{
+      try {
         this.checkForm();
 
         //Check des erreurs dans le formulaire
-        if(this.errors.length > 0){
+        if (this.errors.length > 0) {
           return false;
         } else {
           //Check si c'est une modif ou un ajout
-          if(this.theaterMovieId){
-            const theaterMovieUpdated = {id: this.theaterMovieId, ...theaterMovie}
+          if (this.theaterMovieId) {
+            const theaterMovieUpdated = {
+              id: this.theaterMovieId,
+              ...theaterMovie,
+            };
             await editTheaterMovie(theaterMovieUpdated);
           } else {
             await addTheaterMovie(theaterMovie);
           }
 
           //Redirection
-          this.$router.push('/theater/movies')
+          this.$router.push("/theater/movies");
         }
-      } catch(error){
+      } catch (error) {
+        displayError(error.response);
       }
     },
   },
